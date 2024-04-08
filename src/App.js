def35b2371e5fcd8de9623f954c61e8da7c9e727
5Knight2 <pratiktarale258@gmail.com>
2024-04-08 14:43:39 +0530

    summary added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Header from "./Components/Layout/Header";
+import MealsSummary from "./Components/Meals/MealsSummary";
 function App() {
   return (<React.Fragment>
     <Header></Header>
+    <MealsSummary></MealsSummary>
     <div>
       <h2>Let's get started!</h2>
     </div>
