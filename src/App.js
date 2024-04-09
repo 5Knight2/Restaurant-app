64c84a8ba1cd52073cc740de4fc8632e12f47f10
5Knight2 <pratiktarale258@gmail.com>
2024-04-09 13:20:31 +0530

    Modal Portal added but not dynamic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Header from "./Components/Layout/Header";
 import Meals from './Components/Meals/Meals'
+import Cart from "./Components/Cart/Cart";
 function App() {
   return (<React.Fragment>
+    <Cart></Cart>
     <Header></Header>
     <Meals></Meals>
     <div>
